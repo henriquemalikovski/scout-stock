47475a92d895fc033622c84e78a4ae7c605f6156
Henrique Malikovski <hmalikovski@gmail.com>
2023-10-26 18:25:37 -0300

    ajuste na url de conexão com o banco

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
   try {
-    await mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@scout-stock.n72wxn0.mongodb.net/?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@scout-stock.d0o3hge.mongodb.net/?retryWrites=true&w=majority`);
     console.log('Successfully connected to MongoDB');
   } catch (error) {
     console.log('Failed to connect to MongoDB', error);
